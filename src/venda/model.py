ecc3fdef40efaad054c60a248efd2253f26f1d51
Ana Beatriz Santos <anna.bf.santos1@gmail.com>
2025-07-13 15:43:49 -0300

    feat(venda): adiciona relacionamento entre venda, cliente, produto e vendedor

--- src/venda/model.py
+++ src/venda/model.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, Float, Date, String, ForeignKey
-from sqlalchemy.orm import relationship
 from database import Base
+from sqlalchemy.orm import relationship
 
 class ModeloVenda(Base):
     __tablename__ = "venda"
@@ -8,6 +8,12 @@ class ModeloVenda(Base):
     id = Column(Integer, primary_key=True, index=True)
     data = Column(Date, nullable=False)
     cliente_cpf = Column(String, ForeignKey("cliente.cpf"), nullable=False)
-    vendedor_id = Column(Integer, ForeignKey("vendedor.id"), nullable=False)
+    vendedor_cpf = Column(Integer, ForeignKey("vendedor.cpf"), nullable=False)
+    produto_id = Column(Integer, ForeignKey("produto.id"), nullable=False)
     total = Column(Float, nullable=False, default=0.00)
     comissao = Column(Float, nullable=False, default=0.00)
+    quantidade = Column(Integer, nullable=False, default=1)
+
+    cliente = relationship("ModeloCliente", backref="vendas")
+    vendedor = relationship("ModeloVendedor", backref="vendas")
+    produto = relationship("ModeloProduto", backref="vendas")
